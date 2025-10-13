bbe0aa251795b2866958fd6a42c371131077284f
Legend Paul <qamunyamaina950@gmail.com>
2025-10-13 23:54:37 +0300

    Create sign up page verification

--- controller/signupHandler.js
+++ controller/signupHandler.js
@@ -1,5 +1,46 @@
+const { body, validationResult } = require("express-validator");
+
 const signupGet = (req, res) => {
     res.render("signup");
 };
 
-module.exports = { signupGet };
+const validate = [
+    body("firstName")
+        .trim()
+        .notEmpty()
+        .withMessage("Firstname is required")
+        .isAlpha("en-US", { ignore: " " })
+        .withMessage("Firstname must be only letters"),
+    body("lastName")
+        .trim()
+        .notEmpty()
+        .withMessage("Lastname is required")
+        .isAlpha("en-US", { ignore: " " })
+        .withMessage("Lastname must be only letters"),
+    body("username").trim().notEmpty().withMessage("Username is required"),
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8 })
+        .withMessage("Password must be atleast 8 character long"),
+    body("confirmPassword")
+        .trim()
+        .custom((value, { req }) => value === req.body.password)
+        .withMessage("Password must be equal"),
+];
+
+const signupPost = [
+    validate,
+    (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors.isEmpty());
+        if (!errors.isEmpty()) {
+            console.log(errors.array());
+            return res.render("signup", { error: errors.array()[0].msg });
+        }
+        const { firstName, lastName, username, password } = req.body;
+    },
+];
+
+module.exports = { signupGet, signupPost };
--- routes/signup.js
+++ routes/signup.js
@@ -3,5 +3,6 @@ const signupHandler = require("../controller/signupHandler");
 
 const signupRouter = express.Router();
 signupRouter.get("/", signupHandler.signupGet);
+signupRouter.post("/", signupHandler.signupPost);
 
 module.exports = signupRouter;
