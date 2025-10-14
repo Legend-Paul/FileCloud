8fb088cefdc8de75b8e48fd8fd65f2a1e39eef01
Legend Paul <qamunyamaina950@gmail.com>
2025-10-14 11:33:05 +0300

    Set up session

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const path = require("node:path");
 const homeRouter = require("./routes/home");
 const signupRouter = require("./routes/signup");
 const loginRouter = require("./routes/login");
 const forgotPasswordRouter = require("./routes/forgotPassword");
 const errorHandler = require("./controller/errorHandler");
+const prisma = require("./utils/prisma");
+const passport = require("passport");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
@@ -16,6 +20,24 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+    expressSession({
+        store: new PrismaSessionStore(prisma, {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000, // one day
+        },
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/forgot-password", forgotPasswordRouter);
 app.use("/login", loginRouter);
