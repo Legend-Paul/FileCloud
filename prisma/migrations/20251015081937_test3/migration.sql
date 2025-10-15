11f532a260181556bff3afd5aa7b1f393aa8f17e
Legend Paul <qamunyamaina950@gmail.com>
2025-10-15 11:31:30 +0300

    Create file and folder prisma models

---
+++ prisma/migrations/20251015081937_test3/migration.sql
@@ -0,0 +1,63 @@
+-- CreateEnum
+CREATE TYPE "FolderType" AS ENUM ('DOCUMENT', 'IMAGE', 'VIDEO');
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "parentId" TEXT,
+    "type" "FolderType",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "folderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Folder_parentId_idx" ON "Folder"("parentId");
+
+-- CreateIndex
+CREATE INDEX "Folder_ownerId_idx" ON "Folder"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "Folder_type_idx" ON "Folder"("type");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Folder_parentId_name_ownerId_key" ON "Folder"("parentId", "name", "ownerId");
+
+-- CreateIndex
+CREATE INDEX "File_folderId_idx" ON "File"("folderId");
+
+-- CreateIndex
+CREATE INDEX "File_ownerId_idx" ON "File"("ownerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_folderId_name_key" ON "File"("folderId", "name");
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
