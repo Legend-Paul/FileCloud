bf1dbb2352fe72410f0f14cc9ceb2ae9c5a16ad9
Legend Paul <qamunyamaina950@gmail.com>
2025-10-16 08:12:49 +0300

    Set up multer middleware

--- controller/documentsHandler.js
+++ controller/documentsHandler.js
@@ -1,5 +1,10 @@
+const prisma = require("../utils/prisma");
+
 const documentsGet = (req, res) => {
-    res.render("home", { notHome: true });
+    res.render("home", { path: "/Documents", fileType: "Documents" });
+};
+const documentsNewFile = async (req, res) => {
+    res.send("Files uploaded!");
 };
 
-module.exports = { documentsGet };
+module.exports = { documentsGet, documentsNewFile };
--- routes/documents.js
+++ routes/documents.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const documentsHandler = require("../controller/documentsHandler");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = file.fieldname + "-" + Date.now();
+        cb(null, uniqueSuffix);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 const documentsRouter = express.Router();
 documentsRouter.get("/", documentsHandler.documentsGet);
-// documentsRouter.post("/", documentsHandler.documentsPost);
+documentsRouter.post(
+    "/new/file",
+    upload.array("file", 12),
+    documentsHandler.documentsNewFile
+);
 
 module.exports = documentsRouter;
