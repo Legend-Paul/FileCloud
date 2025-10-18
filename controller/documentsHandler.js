c9fc58a59a77e60f019c17062264745b62d15287
Legend Paul <qamunyamaina950@gmail.com>
2025-10-18 20:28:36 +0300

    Enable file and folder rename

--- controller/documentsHandler.js
+++ controller/documentsHandler.js
@@ -41,7 +41,6 @@ const documentsDelete = async (req, res) => {
     try {
         const path = req.originalUrl;
         const back = path.split("/").slice(0, -1).join("/");
-        console.log(back);
 
         const items = JSON.parse(deleteItems);
         const { folders, files } = items;
@@ -66,9 +65,63 @@ const documentsDelete = async (req, res) => {
     }
 };
 
+const documentRename = async (req, res) => {
+    const path = req.originalUrl;
+    const back = path.split("/").slice(0, -1).join("/");
+    const itemType = path.split("/").at(1).toUpperCase().slice(0, -1); // file or folder
+    const { renameItem, newFileName } = req.body;
+    const { id, type, fileType } = JSON.parse(renameItem);
+    const fileHasExtension = newFileName.includes(".");
+    let finalNewFileName = newFileName;
+    if (type === "files" && !fileHasExtension && fileType) {
+        finalNewFileName = newFileName + "." + fileType;
+    }
+    const item =
+        type === "files"
+            ? await prisma.file.findFirst({
+                  where: {
+                      name: finalNewFileName,
+                      mimeType: fileType,
+                      owner: req.user,
+                  },
+              })
+            : await prisma.folder.findFirst({
+                  where: {
+                      name: newFileName,
+                      owner: req.user,
+                      type: itemType,
+                  },
+              });
+    type === "files"
+        ? await prisma.file.update({
+              where: {
+                  id,
+                  mimeType: fileType,
+                  owner: req.user,
+              },
+              data: {
+                  name: item
+                      ? finalNewFileName + "-" + Date.now()
+                      : finalNewFileName,
+              },
+          })
+        : await prisma.folder.update({
+              where: {
+                  id,
+                  type: itemType,
+                  owner: req.user,
+              },
+              data: {
+                  name: item ? newFileName + "-" + Date.now() : newFileName,
+              },
+          });
+    res.redirect(back);
+};
+
 module.exports = {
     documentsGet,
     documentsNewFile,
     documentsNewFolder,
     documentsDelete,
+    documentRename,
 };
--- routes/documents.js
+++ routes/documents.js
@@ -15,13 +15,14 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const documentsRouter = express.Router();
-documentsRouter.get("/", documentsHandler.documentsGet);
-documentsRouter.post(
-    "/*new/file",
-    upload.array("file", 12),
-    documentsHandler.documentsNewFile
-);
-documentsRouter.post("/*new/folder", documentsHandler.documentsNewFolder);
-documentsRouter.post("/*delete", documentsHandler.documentsDelete);
+// documentsRouter.get("/", documentsHandler.documentsGet);
+// documentsRouter.post(
+//     "/*new/file",
+//     upload.array("file", 12),
+//     documentsHandler.documentsNewFile
+// );
+// documentsRouter.post("/*delete", documentsHandler.documentsDelete);
+// documentsRouter.post("/*new/folder", documentsHandler.documentsNewFolder);
+documentsRouter.post("/*rename", documentsHandler.documentRename);
 documentsRouter.get("/{*splat}", documentsHandler.documentsGet);
 module.exports = documentsRouter;
