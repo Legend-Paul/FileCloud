9f1ad429803096cc7be28fd5d3378c16092f7c7e
Legend Paul <qamunyamaina950@gmail.com>
2025-10-21 16:51:31 +0300

    Add delete, rename, share and view routes at home route

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const homepageHandler = require("../controller/homepageHandler");
+const renameHandler = require("../controller/rename");
+const deleteHandler = require("../controller/delete");
+const viewFileHandler = require("../controller/viewFileHandler");
+const shareHandler = require("../controller/shareHandler");
 
 const homeRouter = express.Router();
 homeRouter.get("/", homepageHandler);
+homeRouter.post("/item/delete", deleteHandler);
+homeRouter.post("/item/rename", renameHandler);
+homeRouter.post("/share/:id", shareHandler);
+homeRouter.get("/view/:id", viewFileHandler);
 
 module.exports = homeRouter;
